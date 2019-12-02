2e2252abd2dd781181b97590cba5c92c9914e8aa
Ana <anaisabel.hortiguela@gmail.com>
2019-12-02 23:35:45 +0000

    1

---
+++ src/com/ana/java/exercise/Array.java
@@ -0,0 +1,37 @@
+package com.ana.java.exercise;
+
+import java.util.Scanner;
+
+public class Array {
+   public int[] array;
+
+   public int getIntergersFromUser (){
+      Scanner reader = new Scanner (System.in);
+      int IntergersReader = reader.nextInt();
+      return 0;
+  }
+
+   public int GetMaxNumber() {
+      return 0;
+   }
+
+   public int getMinNumber() {
+      return 0;
+   }
+
+   public int getOrderedIntergers() {
+      return 0;
+   }
+
+   public int getPrimeIntergers() {
+      return 0;
+   }
+
+   public int getIntergersRepetiedAndTimesOfRepetition() {
+      return 0;
+   }
+}
+
+
+
+
---
+++ src/com/ana/java/exercise/Exercise.java
@@ -0,0 +1,8 @@
+package com.ana.java.exercise;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+    }
+}
