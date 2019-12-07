dce0689fa831610f1dc5c2d3513648a3c0e3a6a4
Ana <anaisabel.hortiguela@gmail.com>
2019-12-07 11:29:31 +0000

    getSizeArray done

--- src/com/ana/java/exercise/Array.java
+++ src/com/ana/java/exercise/Array.java
@@ -3,15 +3,25 @@ package com.ana.java.exercise;
 import java.util.Scanner;
 
 public class Array {
-   public int[] array;
+   private int[] array;
+   private int sizeArray;
 
-   public int getIntergersFromUser (){
-      Scanner reader = new Scanner (System.in);
-      int IntergersReader = reader.nextInt();
-      return 0;
-  }
+   // constructor por defecto
+   public Array() {
+      System.out.println("Te estoy llamando");
+      this.array = new int[0];
+      this.sizeArray = 0;
+   }
 
-   public int GetMaxNumber() {
+   public int getSizeArrayFromUser() {
+      System.out.print("Please the desired lenght of the array here:");
+      Scanner reader = new Scanner(System.in);
+      return reader.nextInt();
+   }
+   public int arrayMaker() {
+   }
+
+   public int getMaxNumber() {
       return 0;
    }
 
@@ -30,7 +40,55 @@ public class Array {
    public int getIntergersRepetiedAndTimesOfRepetition() {
       return 0;
    }
+
+   public int getSizeArray() {
+      return sizeArray;
+   }
+   public void setSizeArray(int sizeArray) {
+      this.sizeArray = sizeArray;
+   }
+   public int getArray() {
+      return sizeArray;
+   }
+   public void setArray(int[]array) {
+      this.array = array;
+   }
 }
+/*
+1 - Leer un array
+   a / leer el tamaño del array
+      1 buscar como leer del usuario.
+      2 pedir/solicitar/pregutnar/que te pregunte por pantalla el numero y leerlo
+      3 devolver lo que he leido
+   b / completar el array del tamaño indicado
+      1 necesito el tamaño. Se lo tengo que pasar por parametro
+      2 asignar tamaño al array
+      3 pedir al usuario que impute numeros HASTA QUE COMPLETE EL ARRAY
+      4 guardar el array.
+   c/ mostrar/pintarlo/... el array
+   d/ obtener maximo
+      1 ...
+      2 ...
+   e/ ....
+   ____________________________________________
+   1 - Leer un array
+   a / leer el tamaño del array
+      1 buscar como leer del usuario.
+      2 pedir/solicitar/pregutnar/que te pregunte por pantalla el numero y leerlo
+      3 devolver lo que he leido
+   b / completar el array del tamaño indicado
+      1 necesito el tamaño. Se lo tengo que pasar por parametro
+      2 asignar tamaño al array
+      3 pedir al usuario que impute numeros HASTA QUE COMPLETE EL ARRAY
+      4 return/devolver.
+   c/ set/get del atributo array
+   d/ mostrar/pintarlo/... el array
+   e/ ºobtener maximo
+   f/ ordernar de menor a mayor
+
+
+
+ */
 
 
 
